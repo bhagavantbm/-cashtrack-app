24845a521e384b6dcd98504590e8258f464f7392
bhagat <bhagavantmuragundi@gmail.com>
2025-05-21 06:39:32 +0530

    Fix duplicate cors declaration

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
 const instance = axios.create({
-baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',});
+  baseURL: API_URL,
+});
 
 instance.interceptors.request.use(config => {
-  const token = localStorage.getItem('token'); // Or your storage
+  const token = localStorage.getItem('token'); 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
+}, error => {
+  return Promise.reject(error);
 });
 
 export default instance;
